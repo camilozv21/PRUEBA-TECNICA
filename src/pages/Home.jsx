b0405a56d2a38f372335c1ed2cc3eb651082e589
camilozv21 <camilozv21@gmail.com>
2023-10-19 14:24:14 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 export const Home = () => {
   return (
     <>
-        <article className="container mb-5">
+        <main className="container mb-5">
             <Link to="/register" className="btn btn-warning fixed-button">Registrarse</Link>
-            <h2 className='container mt-3 mb-3'>Nuestros servicios</h2>
+            <h1 className="container mt-3 mb-3">Restaurante de TSgroup</h1>
             <Carrusel />
             <h2  className='container mt-3 mb-3'>Productos</h2>
             <Cards />
-        </article>
+        </main>
     </>
   )
 }
